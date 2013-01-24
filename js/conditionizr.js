be607fbb15908cdb322020fcf15faffee9ce1e41
Martin Laritz <martin.laritz@gmail.com>
2013-01-23 20:56:01 -0500

    Remove reference to jQuery

--- js/conditionizr.js
+++ js/conditionizr.js
@@ -11,185 +11,196 @@
 	Conditionizr, the conditional free legacy, retina, script and style loader.
 	
 */
-;(function ($) {
+; var conditionizr = function (options) {
 
-	$.fn.conditionizr = function (options) {
-	
-		var settings = {
-			debug      : false,
-			scriptSrc  : 'js/conditionizr/',
-			styleSrc   : 'css/conditionizr/',
-			ieLessThan : { active: false, version: '9', scripts: false, styles: false, classes: true, customScript: 'none'},
-			chrome     : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			safari     : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			opera      : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			firefox    : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			ie10       : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			ie9        : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			ie8        : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			ie7        : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			ie6        : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			retina     : { scripts: false, styles: false, classes: true, customScript: 'none' },
-			mac    : true,
-			win    : true,
-			x11    : true,
-			linux  : true
-		};
-		
-		if (options) {
-			$.extend(settings, options);
-		}
+    var settings = {
+        debug: false,
+        scriptSrc: 'js/conditionizr/',
+        styleSrc: 'css/conditionizr/',
+        ieLessThan: { active: false, version: '9', scripts: false, styles: false, classes: true, customScript: 'none' },
+        chrome: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        safari: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        opera: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        firefox: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        ie10: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        ie9: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        ie8: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        ie7: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        ie6: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        retina: { scripts: false, styles: false, classes: true, customScript: 'none' },
+        mac: true,
+        win: true,
+        x11: true,
+        linux: true
+    };
 
-		return this.each(function () {
+    //thanks to Markus on StackOverflow
+    function mergeRecursive(obj1, obj2) {
 
-			function conditionizrLoader() {
-				$.each(browserSettings, function (resourceType, val) {
-						
-					if (resourceType === 'classes' && val === true) {
-						document.getElementsByTagName('html')[0].className += ' ' + theBrowser;
-					}
-					
-					if (resourceType === 'scripts' && val === true) {
-						var scriptTag = document.createElement('script');
-						scriptTag.src = settings.scriptSrc + theBrowser + '.js';
-						document.getElementsByTagName('head')[0].appendChild(scriptTag);
-					}
-					
-					if (resourceType === 'styles' && val === true) {
-						var linkTag = document.createElement('link');
-						linkTag.rel = 'stylesheet';
-						linkTag.href = settings.styleSrc + theBrowser + '.css';
-						document.getElementsByTagName('head')[0].appendChild(linkTag);
-					}
-					
-					if (resourceType === 'customScript' && val != 'none') {
-						var customScriptTag = document.createElement('script');
-						customScriptTag.src = browserSettings.customScript;
-						document.getElementsByTagName('head')[0].appendChild(customScriptTag);
-					}
-					
-				});
-			}
-		
-			document.getElementsByTagName('html')[0].id = 'conditionizr';
-		
-			var browsers = [
-				{'testWith': 'chrome', 'testSettings': settings.chrome}, 
-				{'testWith': 'safari', 'testSettings': settings.safari},
-				{'testWith': 'firefox', 'testSettings': settings.firefox},
-				{'testWith': 'opera', 'testSettings': settings.opera}
-			];
-			
-			for (var i = 0; i < browsers.length; i++) {
+        for (var p in obj2) {
+            try {
+                if (obj2[p].constructor == Object) {
+                    obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+                } else {
+                    obj1[p] = obj2[p];
+                }
+            } catch (e) {
+                obj1[p] = obj2[p];
+            }
+        }
 
-				var currentBrowser = browsers[i];
+        return obj1;
+    }
 
-				if (navigator.userAgent.toLowerCase().indexOf(currentBrowser.testWith) > -1) {
-					var browserSettings = currentBrowser.testSettings,
-						theBrowser = currentBrowser.testWith;
-						conditionizrLoader();
-					break;
-				}
+    if (options) {
+        mergeRecursive(settings, options);
+    }
 
-			}
-			
-			function getIEVersion() {
+    function conditionizrLoader() {
+        for (var resourceType in browserSettings) {
+            var val = browserSettings[resourceType];
 
-				var rv = -1;
+            if (resourceType === 'classes' && val === true) {
+                document.getElementsByTagName('html')[0].className += ' ' + theBrowser;
+            }
 
-				if (navigator.appName == 'Microsoft Internet Explorer') {
-					var ua = navigator.userAgent,
-						re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-					
-					if (re.exec(ua) != null) {
-						rv = parseFloat(RegExp.$1);
-					}
-				}
+            if (resourceType === 'scripts' && val === true) {
+                var scriptTag = document.createElement('script');
+                scriptTag.src = settings.scriptSrc + theBrowser + '.js';
+                document.getElementsByTagName('head')[0].appendChild(scriptTag);
+            }
 
-				return rv;
+            if (resourceType === 'styles' && val === true) {
+                var linkTag = document.createElement('link');
+                linkTag.rel = 'stylesheet';
+                linkTag.href = settings.styleSrc + theBrowser + '.css';
+                document.getElementsByTagName('head')[0].appendChild(linkTag);
+            }
 
-			}
-			
-			var version = getIEVersion();
-			
-			if (version > -1) {
-			
-				if (version < settings.ieLessThan.version + '.0') {
-			
-					var theBrowser = 'lt-ie' + settings.ieLessThan.version,
-						browserSettings = settings.ieLessThan;
-					
-					conditionizrLoader();
-					
-				}
-			
-				if (version === 10.0) {
-					var browserSettings = settings.ie10;
-				}
-				else if (version === 9.0) {
-					var browserSettings = settings.ie9;
-				}
-				else if (version === 8.0) {
-					var browserSettings = settings.ie8;
-				}
-				else if (version === 7.0) {
-					var browserSettings = settings.ie7;
-				}
-				else if (version === 6.0) {
-					var browserSettings = settings.ie6;
-				}
-				
-				var theBrowser = 'ie' + version;
-				
-				conditionizrLoader();
-				
-			}
+            if (resourceType === 'customScript' && val != 'none') {
+                var customScriptTag = document.createElement('script');
+                customScriptTag.src = browserSettings.customScript;
+                document.getElementsByTagName('head')[0].appendChild(customScriptTag);
+            }
 
-			if (window.devicePixelRatio >= 2) {
-			
-				var browserSettings = settings.retina,
-					theBrowser = 'retina';
-					
-				conditionizrLoader();
-				
-			}
-			
-			else {
-				document.getElementsByTagName('html')[0].className += ' no-retina';
-			}
-			
-			var oSys = [
-				{'testWith': 'Win', 'testSettings': settings.win}, 
-				{'testWith': 'Mac', 'testSettings': settings.mac}, 
-				{'testWith': 'X11', 'testSettings': settings.x11}, 
-				{'testWith': 'Linux', 'testSettings': settings.linux}
-			];
-			
-			for (var i = 0; i < oSys.length; i++) {
-				var currentPlatform = oSys[i];
-				if (navigator.appVersion.indexOf(currentPlatform.testWith) > -1) {
+        }
+    }
 
-					var osSettings = currentPlatform.testSettings,
-						theOS = currentPlatform.testWith;
+    document.getElementsByTagName('html')[0].id = 'conditionizr';
 
-					if (osSettings === true) {
-						document.getElementsByTagName('html')[0].className += ' ' + currentPlatform.testWith.toLowerCase();
-					}
+    var browsers = [
+        { 'testWith': 'chrome', 'testSettings': settings.chrome },
+        { 'testWith': 'safari', 'testSettings': settings.safari },
+        { 'testWith': 'firefox', 'testSettings': settings.firefox },
+        { 'testWith': 'opera', 'testSettings': settings.opera }
+    ];
 
-					break;
-				}
-			}
-			
-			if (settings.debug === true) {
-				console.log('Start Conditionizr Debug\n');
-				console.log('Script location: ' + settings.scriptSrc);
-				console.log('Style location: ' + settings.styleSrc);
-				console.log('Browser: ' + theBrowser);
-				console.log('OS: ' + theOS);
-				console.log('\nEnd Conditionizr Debug\n');
-			}
-			
-		});
-	};
-	
-})(jQuery);
+    for (var i = 0; i < browsers.length; i++) {
+
+        var currentBrowser = browsers[i];
+
+        if (navigator.userAgent.toLowerCase().indexOf(currentBrowser.testWith) > -1) {
+            var browserSettings = currentBrowser.testSettings,
+                theBrowser = currentBrowser.testWith;
+            conditionizrLoader();
+            break;
+        }
+
+    }
+
+    function getIEVersion() {
+
+        var rv = -1;
+
+        if (navigator.appName == 'Microsoft Internet Explorer') {
+            var ua = navigator.userAgent,
+                re = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+
+            if (re.exec(ua) != null) {
+                rv = parseFloat(RegExp.$1);
+            }
+        }
+
+        return rv;
+
+    }
+
+    var version = getIEVersion();
+
+    if (version > -1) {
+
+        if (version < settings.ieLessThan.version + '.0') {
+
+            var theBrowser = 'lt-ie' + settings.ieLessThan.version,
+                browserSettings = settings.ieLessThan;
+
+            conditionizrLoader();
+
+        }
+
+        if (version === 10.0) {
+            var browserSettings = settings.ie10;
+        }
+        else if (version === 9.0) {
+            var browserSettings = settings.ie9;
+        }
+        else if (version === 8.0) {
+            var browserSettings = settings.ie8;
+        }
+        else if (version === 7.0) {
+            var browserSettings = settings.ie7;
+        }
+        else if (version === 6.0) {
+            var browserSettings = settings.ie6;
+        }
+
+        var theBrowser = 'ie' + version;
+
+        conditionizrLoader();
+
+    }
+
+    if (window.devicePixelRatio >= 2) {
+
+        var browserSettings = settings.retina,
+            theBrowser = 'retina';
+
+        conditionizrLoader();
+
+    }
+
+    else {
+        document.getElementsByTagName('html')[0].className += ' no-retina';
+    }
+
+    var oSys = [
+        { 'testWith': 'Win', 'testSettings': settings.win },
+        { 'testWith': 'Mac', 'testSettings': settings.mac },
+        { 'testWith': 'X11', 'testSettings': settings.x11 },
+        { 'testWith': 'Linux', 'testSettings': settings.linux }
+    ];
+
+    for (var i = 0; i < oSys.length; i++) {
+        var currentPlatform = oSys[i];
+        if (navigator.appVersion.indexOf(currentPlatform.testWith) > -1) {
+
+            var osSettings = currentPlatform.testSettings,
+                theOS = currentPlatform.testWith;
+
+            if (osSettings === true) {
+                document.getElementsByTagName('html')[0].className += ' ' + currentPlatform.testWith.toLowerCase();
+            }
+
+            break;
+        }
+    }
+
+    if (settings.debug === true) {
+        console.log('Start Conditionizr Debug\n');
+        console.log('Script location: ' + settings.scriptSrc);
+        console.log('Style location: ' + settings.styleSrc);
+        console.log('Browser: ' + theBrowser);
+        console.log('OS: ' + theOS);
+        console.log('\nEnd Conditionizr Debug\n');
+    }
+}
